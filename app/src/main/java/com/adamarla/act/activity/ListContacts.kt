845d0c3b156f18a52d9e5eba5d653bdf66ef6a44
Akshay Damarla <akshay.damarla@gmail.com>
2017-09-27 00:47:22 -0500

    basic filter

--- app/src/main/java/com/adamarla/act/activity/ListContacts.kt
+++ app/src/main/java/com/adamarla/act/activity/ListContacts.kt
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import com.adamarla.act.ACTAD
 import com.adamarla.act.R
 import com.adamarla.act.data.Contact
@@ -21,8 +24,22 @@ class ListContacts : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_contacts)
+
         val toolbar = findViewById(R.id.tbMain) as Toolbar?
         setSupportActionBar(toolbar)
+
+        val etFilter = findViewById(R.id.etFilter) as EditText
+        etFilter.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+            override fun afterTextChanged(editable: Editable?) {
+                if (!editable.isNullOrBlank()) {
+                    var contacts = contactsBox.query().build().find()
+                    contacts = contacts.filter { it.toString().contains(editable.toString(), true) }
+                    (recyclerView.adapter as ContactListAdapter).updateContacts(contacts)
+                }
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+        })
 
         recyclerView = findViewById(R.id.rvList) as RecyclerView
         recyclerView.setHasFixedSize(true)
@@ -31,8 +48,8 @@ class ListContacts : AppCompatActivity() {
 
         contactsBox = (application as ACTAD).boxStore.boxFor(Contact::class.java)
 
-        val contacts: List<Contact> = contactsBox.query().build().find()
-        val adapter = ContactListAdapter(this, contacts)
+        val contacts = contactsBox.query().build().find()
+        val adapter = ContactListAdapter(this, contacts.sortedBy { it.fullName })
         recyclerView.adapter = adapter
 
         val fab = findViewById(R.id.fab) as FloatingActionButton?
